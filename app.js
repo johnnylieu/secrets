e3420e57a7ec016667d1e2c5c3d9db130da6eb74
Johnny <JohnnyLieu@yahoo.com>
2022-12-29 11:51:39 -0800

    user able to sing up and log in, damn I'm good, figured out the login functionality on my own with no google or youtube, just pure logic

--- app.js
+++ app.js
@@ -11,9 +11,52 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
+mongoose.connect('mongodb://localhost:27017/userDB');
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = mongoose.model('User', userSchema);
+
 app.get('/', function(req, res){
-    res.send(`<h1>HELLO WORD!</h1>`);
+    res.render('home');
 });
+
+app.route('/login')
+    .get(function(req, res){
+        res.render('login');
+    })
+    .post(function(req, res){
+        User.findOne({email: req.body.username}, function(err, result){
+            if (result) {
+                if (result.password === req.body.password) {
+                    res.render(`secrets`);
+                } else {res.send(`<h1>Incorrect password, please try again.</h1>`)}
+            } else if (!result) {
+                {res.send(`<h1>No such user exists, please check your spelling or sign up.</h1>`);}
+            } else if (err) {
+                res.send(err);
+            };
+        });
+    });
+
+app.route('/register')
+    .get(function(req, res){
+        res.render('register');
+    })
+    .post(function(req, res){
+        newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save(function(err, result){
+            if (!err) {
+                res.render('secrets');
+            } else {res.send(err);}
+        });
+    });
 
 app.listen(port, function(){
     console.log(`Listening on port ${port}`);
