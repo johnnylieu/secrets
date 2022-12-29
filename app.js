8655bd1b4804564d5cbb3c566980e52506d88e66
Johnny <JohnnyLieu@yahoo.com>
2022-12-29 14:56:34 -0800

    able to encrypt user password when storing to mongoDB now

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+const secret = require('./noneyo');
 
 port = process.env.PORT || 3000;
 
@@ -13,10 +15,12 @@ app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost:27017/userDB');
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-};
+});
+
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
 
 const User = mongoose.model('User', userSchema);
 
